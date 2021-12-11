73412f8437820e848a589f151ad362881b654c26
MechEngMalik <mecheng.malik93@gmail.com>
2021-12-11 23:31:18 +0200

    base-steps

---
+++ app/src/main/java/com/example/color_harmony/PaletteAdapter.java
@@ -0,0 +1,58 @@
+package com.example.color_harmony;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amplifyframework.datastore.generated.model.Palette;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaletteAdapter extends RecyclerView.Adapter<PaletteAdapter.PaletteViewHolder>{
+    List<Palette> allPalette = new ArrayList<Palette>();
+
+
+    public PaletteAdapter(List<Palette> allPalette){
+        this.allPalette = allPalette;
+    }
+    public static class PaletteViewHolder extends RecyclerView.ViewHolder {
+        public Palette palette;
+        public View itemView;
+
+        public PaletteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public PaletteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_palette,parent,false);
+        PaletteViewHolder paletteViewHolder = new PaletteViewHolder(view);
+
+
+        return paletteViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PaletteViewHolder holder, int position) {
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return allPalette.size();
+    }
+
+
+}
