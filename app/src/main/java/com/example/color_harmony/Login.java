4703b5270dd880829ea47a8aa46303f159e444c3
obada-naser <obadanaser135@gmail.com>
2021-12-12 10:24:22 +0200

    login page basics

--- app/src/main/java/com/example/color_harmony/Login.java
+++ app/src/main/java/com/example/color_harmony/Login.java
@@ -3,6 +3,10 @@ package com.example.color_harmony;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.amplifyframework.core.Amplify;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+        Amplify.Auth.fetchAuthSession(
+                result -> Log.i("AmplifyQuickstart", result.toString()),
+                error -> Log.e("AmplifyQuickstart", error.toString())
+        );
+
+        EditText userName=findViewById(R.id.userName);
+        EditText password=findViewById(R.id.password);
+
+
+
+
+
+
+
+
     }
 }
