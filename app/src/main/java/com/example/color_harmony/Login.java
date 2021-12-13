618b777cb3d78ea7d5db9ac0d9f928d3254c0d5f
obada-naser <obadanaser135@gmail.com>
2021-12-13 10:53:06 +0200

    adding the sign up

--- app/src/main/java/com/example/color_harmony/Login.java
+++ app/src/main/java/com/example/color_harmony/Login.java
@@ -2,9 +2,13 @@ package com.example.color_harmony;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.amplifyframework.core.Amplify;
 
@@ -17,18 +21,45 @@ public class Login extends AppCompatActivity {
 
 
 
-        Amplify.Auth.fetchAuthSession(
-                result -> Log.i("AmplifyQuickstart", result.toString()),
-                error -> Log.e("AmplifyQuickstart", error.toString())
-        );
+//        Amplify.Auth.fetchAuthSession(
+//                result -> Log.i("AmplifyQuickstart", result.toString()),
+//                error -> Log.e("AmplifyQuickstart", error.toString())
+//        );
 
         EditText userName=findViewById(R.id.userName);
         EditText password=findViewById(R.id.password);
 
 
+        Button loginButton=findViewById(R.id.loginButton);
 
 
+        TextView signUpText=findViewById(R.id.signUpText);
 
+
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent goSignUp=new Intent(Login.this,SignUp.class);
+                startActivity(goSignUp);
+
+
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goMain=new Intent(Login.this,MainActivity.class);
+                Amplify.Auth.signIn(
+                        userName.getText().toString(),
+                        password.getText().toString(),
+                        result -> Log.i("AuthQuickstart", result.isSignInComplete() ? "Sign in succeeded" : "Sign in not complete"),
+                        error -> Log.e("AuthQuickstart", error.toString())
+                );
+                startActivity(goMain);
+            }
+        });
 
 
 
