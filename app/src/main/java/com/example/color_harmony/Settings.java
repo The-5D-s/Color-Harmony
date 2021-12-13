e423f5f40bbaaf80a1f0067d50ff8244ea8c9038
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-12-13 03:15:08 +0200

    almost finished, need to link the activities all togather, dark mode is done, shre button is added, new activities to edit the profile are added without linking any thing yet.

--- app/src/main/java/com/example/color_harmony/Settings.java
+++ app/src/main/java/com/example/color_harmony/Settings.java
@@ -1,14 +1,66 @@
 package com.example.color_harmony;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.app.UiModeManager.MODE_NIGHT_YES;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
 
+    Switch mode;
+    Button share;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.Theme_Dark);
+        }
+        else {
+            setTheme(R.style.Theme_Light);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-    }
+
+        mode = findViewById(R.id.mode);
+        share = findViewById(R.id.share);
+
+        mode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+
+                String body = "Share our app with your friends!";
+                String sub = "http://play.google.com";
+
+                intent.putExtra(Intent.EXTRA_TEXT, body);
+                intent.putExtra(Intent.EXTRA_TEXT, sub);
+
+                startActivity(Intent.createChooser(intent, "Share"));
+
+            }
+        });
+        }
 }
