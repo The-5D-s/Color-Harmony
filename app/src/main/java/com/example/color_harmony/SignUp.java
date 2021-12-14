0a0e37f256e54b693eee2ac529beafe1f7b110f2
ahmedbani <a13banisal@gmail.com>
2021-12-14 17:11:11 +0200

    saving to DB

--- app/src/main/java/com/example/color_harmony/SignUp.java
+++ app/src/main/java/com/example/color_harmony/SignUp.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import com.amplifyframework.auth.AuthUserAttributeKey;
 import com.amplifyframework.auth.options.AuthSignUpOptions;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.User;
 
 public class SignUp extends AppCompatActivity {
 
@@ -34,7 +35,7 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent confirm=new Intent(SignUp.this,SignUpConfirm.class);
+                Intent confirm=new Intent(SignUp.this,Login.class);
                 AuthSignUpOptions options = AuthSignUpOptions.builder()
                         .userAttribute(AuthUserAttributeKey.email(), email.getText().toString())
                         .userAttribute(AuthUserAttributeKey.name(), userName.getText().toString())
@@ -42,6 +43,14 @@ public class SignUp extends AppCompatActivity {
                 Amplify.Auth.signUp(userName.getText().toString(), password.getText().toString(), options,
                         result -> Log.i("AuthQuickStart", "Result: " + result.toString()),
                         error -> Log.e("AuthQuickStart", "Sign up failed", error)
+                );
+                User item = User.builder()
+                        .name(userName.getText().toString())
+                        .build();
+                Amplify.DataStore.save(
+                        item,
+                        success -> Log.i("Amplify", "Saved item: " + success.item().getId()),
+                        error -> Log.e("Amplify", "Could not save item to DataStore", error)
                 );
                 startActivity(confirm);
             }
